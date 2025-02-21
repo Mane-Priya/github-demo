2f254e13103143d25affc3c99516c401fa0ddfb4
Priyanka Mane <manepriyanka173@gmail.com>
2025-02-21 15:16:36 -0500

    Second Commit

--- Demo-1.py
+++ Demo-1.py
@@ -1,7 +1,6 @@
 print(2+7)
 print(15 - 5)
-print(10*2)
-print(56/6)
+print(50/6)
 print(5%2)
 print(2 ** 5)     #This will give 2^5 = (2*2*2*2*2) = 32
 print(19//3)      # This will make division(19/3) and give closet round no as an ans here ans = 6
